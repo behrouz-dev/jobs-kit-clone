3bf56053eab10127f034906ce282335aca3d4de5
behrouz <bmb.mm7@gmail.com>
2025-02-05 12:33:31 +0330

    new scripts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,13 +2,4 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma: PrismaClient = new PrismaClient();
 
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!(global as any).prisma) {
-//     (global as typeof globalThis & { prisma?: PrismaClient }).prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
-
 export default prisma;
